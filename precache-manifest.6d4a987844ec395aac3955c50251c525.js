0b9c715714596d0e1914f0e3ab97afee141eb11c
rduncan <rduncan@phpatlanta.com>
2019-01-03 18:28:05 -0500

    Updates

---
+++ precache-manifest.6d4a987844ec395aac3955c50251c525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52bdf5a2c58bc6c1c64f",
+    "url": "/recipe-react/static/css/main.6ee5d87d.chunk.css"
+  },
+  {
+    "revision": "52bdf5a2c58bc6c1c64f",
+    "url": "/recipe-react/static/js/main.52bdf5a2.chunk.js"
+  },
+  {
+    "revision": "0289cad058faf1c70973",
+    "url": "/recipe-react/static/css/1.c5d65dc1.chunk.css"
+  },
+  {
+    "revision": "0289cad058faf1c70973",
+    "url": "/recipe-react/static/js/1.0289cad0.chunk.js"
+  },
+  {
+    "revision": "5edaa3c6eff396868abb",
+    "url": "/recipe-react/static/js/runtime~main.5edaa3c6.js"
+  },
+  {
+    "revision": "908bfbc3b1805f6c47680f34e7f0f743",
+    "url": "/recipe-react/index.html"
+  }
+];
